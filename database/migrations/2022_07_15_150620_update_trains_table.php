21897f5efb7cbea7e782147b9c22376597a13223
Giovanni Ciotta <giociotta@gmail.com>
2022-07-15 17:22:04 +0200

    first commit

---
+++ database/migrations/2022_07_15_150620_update_trains_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->tinyInteger('Codice Treno');
+            $table->tinyInteger('Numero Carrozze');
+            $table->string('In orario');
+            $table->string('Cancellato');
+
+
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            //
+        });
+    }
+}
